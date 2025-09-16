3e7d7105846dc68bf80e4f945017506e9018d382
Steven Loy <231585531+sloy1cosc381@users.noreply.github.com>
2025-09-15 20:46:58 -0400

    Added heroc conclusion. (Also realized I messed up saving the first time but it's fine everything's good now I'll just need to re add the center stuff for the other branch)

--- story.py
+++ story.py
@@ -12,7 +12,8 @@ def intro():
 
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
-    print("You try to pull the sowrd out, but instead take the whole thing out of the ground.")
+    print("You try to pull the sword out, but instead take the whole thing out of the ground.")
+    print("This sword-in-stone is more like a hammer now, but you wield it valiently none the less.")
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
