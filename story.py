14748310d3343ec3c53afdfa2c18024a6067019e
Steven Loy <231585531+sloy1cosc381@users.noreply.github.com>
2025-09-16 01:04:34 +0000

    Added evil conclusion

--- story.py
+++ story.py
@@ -16,6 +16,7 @@ def left_path():
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
     print("You kick the squirrel, easily sending it flying.")
+    print("You're filled with a dark need, and hunt down and kick every animal in land.")
 
 def center_path():
     print("You walk down the center and into a clearing with a pond. Something glitters at the bottom.")
