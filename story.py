ff9dd8499784c39cdfd8b2c2874a9cd95bbbea9e
Steven Loy <231585531+sloy1cosc381@users.noreply.github.com>
2025-09-15 20:43:17 -0400

    Added sword aquisition text.

--- story.py
+++ story.py
@@ -5,14 +5,20 @@ def intro():
         left_path()
     elif choice == "right":
         right_path()
+    elif choice == "center":
+        center_path()
     else:
         print("You stand still, unsure what to do. The forest swallows you.")
 
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
+    print("You try to pull the sowrd out, but instead take the whole thing out of the ground.")
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
+    
+def center_path():
+    print("You walk down the center and into a clearing with a pond. Something glitters at the bottom.")
 
 if __name__ == "__main__":
     intro()
